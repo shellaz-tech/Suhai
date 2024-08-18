40f87813d110ed8fafa283b01be4017187c60d48
shellaz-tech <svinuraishelton78@gmail.com>
2024-08-19 01:47:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
 global.read_status = process.env.AUTO_READ_STATUS || "true"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254786273945,254702365210,254708336448,254762508828";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "263784055079,263784055079,263784055079,263784055079";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://sessionpair.onrender.com";
